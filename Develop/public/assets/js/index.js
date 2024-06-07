6f55aff4ffc0a3b855133c39490b3c823d6e3faf
Ethan <ethansengpaseuth@gmail.com>
2024-06-07 00:07:45 -0600

    almost complete;

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,3 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.statis('public'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+app.get('/api/notes', (req, res) => {
+  res.json([]);
+});
+app.post('/api/notes', (req, res) => {
+  res.json(req.body);
+});
+app.delete('/api/notes', (req, res) => {
+  res.json({ id: req.params.id });
+});
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 let noteForm;
 let noteTitle;
 let noteText;
